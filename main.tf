86715f8c4611591e32498ff6a51b4f3b5b5eadf3
VivekGuvvala <vivekguvvala7@gmail.com>
2026-05-08 23:23:51 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+    # source = "../terraform-AWS-VPC"
+    source = "https://github.com/VivekGuvvala/vpc-module-test.git?ref=main"
+    # vpc_cidr = "10.0.0.0/16"
+    # project_name = "roboshop"
+    # environment = "dev"
+
+    vpc_cidr = var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    vpc_tags = var.vpc_tags
+    igw_tags = var.igw_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+
+    is_peering_required = true
+}
+
+# data "aws_availability_zones" "available" {
+#   state = "available"
+# }
